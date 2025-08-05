235cc7472a553397c6ca4d87961954c8a14695ae
Steven Nguyen <43599068+sdnguyen98@users.noreply.github.com>
2025-08-05 12:22:23 -0700

    Update gym.py
    Updated Selenium functions and code to replace deprecated methods

--- gym.py
+++ gym.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 import time
 
-PATH = "C:\Program Files\chromedrive\chromedriver.exe"
-driver = webdriver.Chrome(PATH)
+# Use Service class for ChromeDriver
+service = Service("C:\\chromedriver\\chromedriver.exe")
+driver = webdriver.Chrome(service=service)
 
 #YOUR OSU LOGIN
 login_u = input("Enter username:")
@@ -13,12 +16,13 @@ driver.get("https://shop.recsports.oregonstate.edu/booking/47c87f69-0234-4aba-9c
 
 time.sleep(2)
 
-login_button = driver.find_element_by_class_name("loginOption.btn.btn-lg.btn-block.btn-social.btn-soundcloud")
+# Updated element finding methods
+login_button = driver.find_element(By.CLASS_NAME, "loginOption.btn.btn-lg.btn-block.btn-social.btn-soundcloud")
 login_button.click()
 
-userlog = driver.find_element_by_id("username")
+userlog = driver.find_element(By.ID, "username")
 userlog.send_keys(login_u)
-passlog = driver.find_element_by_id("password")
+passlog = driver.find_element(By.ID, "password")
 passlog.send_keys(login_p)
 passlog.send_keys(Keys.RETURN)
 
@@ -29,7 +33,7 @@ print(current_link)
 flag = 0
 while(flag == 0):
     if(current_link == "https://shop.recsports.oregonstate.edu/booking/47c87f69-0234-4aba-9c22-99d036560eb8" ):
-        date_buttons = driver.find_elements_by_class_name("btn.btn-default.single-date-select-button.single-date-select-one-click")
+        date_buttons = driver.find_elements(By.CLASS_NAME, "btn.btn-default.single-date-select-button.single-date-select-one-click")
         flag = 1
     else:
         time.sleep(5)
